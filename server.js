a9f78f69137b74b291ff138f340ac2f95e1ed07f
kcerbulis <kcerbulis@gmail.com>
2019-04-30 10:44:01 -0700

    Added constructor functions for all intended API's

--- server.js
+++ server.js
@@ -15,6 +15,59 @@ const app = express();
 
 
 
+
+
+// ======================================================
+// ----------------Constructor Function------------------
+// ======================================================
+
+// The right side of the constructor will need to be changed in order to fit the API result
+function Geocode (seachQuery){
+  this.adress = seachQuery.adress;
+  this.originLat = seachQuery.originLat;
+  this.originalLong = seachQuery.originalLong;
+}
+
+function Places (seachQuery){
+  this.destination = seachQuery.destination;
+  this.destLat = seachQuery.destLat;
+  this.destLong = seachQuery.destLong;
+  this.name = seachQuery.name;
+  this.prices = seachQuery.prices;
+  this.rating = seachQuery.rating;
+  this.openNow = seachQuery.openNow;
+}
+
+function DarkSky (seachQuery){
+  this.currentIcon = seachQuery.currentIcon;
+  this.currentTemp = seachQuery.currentTemp;
+}
+
+function Sunset (seachQuery){
+  this.sunsetTime = seachQuery.sunsetTime;
+}
+
+function Zomato (seachQuery) {
+  this.menu = seachQuery.menu;
+}
+
+function Uber (seachQuery){
+  this.priceEstimates = seachQuery.priceEstimates;
+}
+
+function Routes (seachQuery){
+  this.distance = seachQuery.distance;
+  this.durration = seachQuery.durration;
+}
+
+
+
+
+
+
+
+
+
 // Middlewear - bridges between data and application
 app.set('view-engine', 'ejs');
 
